054ceff7e8b685f63e765ea9c2858c917030cb8b
Ilija Trokovski <ilija.trokovski@gmail.com>
2022-06-23 00:59:49 +0200

    Initial commit

---
+++ Models/Domain/Burger.cs
@@ -0,0 +1,30 @@
+﻿namespace BurgerApp.Models.Domain
+{
+    public class Burger
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public bool IsVegeterian { get; set; }
+
+        public bool IsVegan { get; set; }
+
+        public bool HasFries { get; set; }
+
+        public Burger(string name,decimal price,bool isVegetrian,bool isVegan,bool hasFries)
+        {
+            if(name is null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            Name = name;
+            Price = price;
+            IsVegeterian = isVegetrian;
+            IsVegan = isVegan;
+            HasFries = hasFries;
+        }
+
+    }
+}
---
+++ Models/Domain/Order.cs
@@ -0,0 +1,37 @@
+﻿namespace BurgerApp.Models.Domain
+{
+    public class Order
+    {
+        public string FullName { get; set; }
+
+        public string Address { get; set; }
+
+        public bool IsDelivered { get; private set; }
+
+        public string Location { get; set; }
+
+        public IEnumerable<Burger> Burgers { get; set; } = new List<Burger>();
+
+        public Order (string fullname,string adress,bool isDelivered,string location,IEnumerable<Burger> burgers)
+        {
+            FullName = fullname;
+            Address = adress;
+            IsDelivered = false;
+            Location = location;
+            Burgers = burgers;
+        }
+
+        public void Deliver()
+        {
+            if (!IsDelivered)
+            {
+                throw new Exception(" Can not be delivered");
+            }
+
+            IsDelivered = true;
+        }
+
+        
+    }
+}
+
